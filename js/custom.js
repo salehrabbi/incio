1f155a0abe92a9b48b6e22e7f5b909bc314799f0
S.M Saleh Rabbi <sm.salehrabbi@gmail.com>
2018-04-13 16:31:13 +0600

    slider youtube

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,50 @@
+$(".noticias .item .date").append('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.033 17.801"><defs></defs><path id = "Path_548" data - name = "Path 548" class = "cls-1" d = "M0,0H111.033l-8.865,17.668L0,17.8Z" / ></svg>');
+
+$(".section-head").append('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46.669 46.669"><defs></defs><rect id="Rectangle_739" data-name="Rectangle 739" class="cls-1" width="33" height="33" transform="translate(23.334) rotate(45)"/></svg>');
 
 
+jQuery('iframe[src*="https://www.youtube.com/embed/"]').addClass("youtube-iframe");
+
+jQuery(".youtube .youtube-nav").click(function () {
+    // changes the iframe src to prevent playback or stop the video playback in our case
+    $('.youtube-iframe').each(function (index) {
+        $(this).attr('src', $(this).attr('src'));
+        return false;
+    });
+
+    //click function
+});
+
+$('.slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    infinite: false,
+    asNavFor: '.slick2'
+});
+$('.slick2').slick({
+    slidesToShow: 25,
+    slidesToScroll: 1,
+    asNavFor: '.slick',
+    dots: false,
+    infinite: false,
+    centerMode: false,
+    focusOnSelect: false
+});
+
+
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<i class="fas fa-angle-left"></i>',
+    nextArrow: '<i class="fas fa-angle-right"></i>',
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    fade: true,
+    infinite: true,
+});
